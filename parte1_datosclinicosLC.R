6c0a83971e7c19ef2b82edc8cd10fb9ef3580db9
ximena <xvc0211@hotmail.com>
2025-01-28 10:50:27 -0500

    Initial setup

---
+++ parte1_datosclinicosLC.R
@@ -0,0 +1,17 @@
+x = 2
+
+#x es igual a 2
+setwd("C:/Users/xavie/Documents/LungCanTME")
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("curatedTCGAData")
+
+#abrir la libreria de TCGA utils
+library("TCGAutils")
+library("curatedTCGAData")
+luad <- curatedTCGAData(
+  diseaseCode = "LUAD", assays = c("CNASeq", "Mutation", "miRNA*",
+                                   "RNASeq2*", "mRNAArray", "Methyl*"), version = "1.1.38", dry.run = FALSE
+)
+#revisar la cant de pacientes por datos moleculares
+sampleTables(luad)
